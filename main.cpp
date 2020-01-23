045e23ca9649ac6d5ec9fa107422d82149989203
haolipeng <1078285863@qq.com>
2020-01-23 12:50:26 +0800

    二维数组的引用

--- main.cpp
+++ main.cpp
@@ -57,6 +57,37 @@ void test_biset_tostring()
     cout << s <<endl;
 }
 
+//推导出数组的维数
+template <size_t R, size_t C, typename T>
+void init1(T a[R][C])
+{
+    for (size_t i= 0; i < R; i++) {
+        for(size_t j = 0; j < C; j++)
+        {
+            a[i][j] = T();
+        }
+    }
+}
+
+//a声明是一个二维数组的引用
+template <size_t R, size_t C, typename T>
+void init2(T (&a) [R][C])
+{
+    for (size_t i= 0; i < R; i++) {
+        for(size_t j = 0; j < C; j++)
+        {
+            a[i][j] = T();
+        }
+    }
+}
+
+void test_ref_array()
+{
+    int a[10][20];
+    init1<10, 20>(a);
+    init2(a);
+}
+
 int main() {
     //use a vector
     Container<int, vector> container;
